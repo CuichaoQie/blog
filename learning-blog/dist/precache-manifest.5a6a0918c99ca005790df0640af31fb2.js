8e5be98e50a69543aea9cbfc2637795b53a48f54
cuichao <cuichao.qie@xiaozao.org>
2020-04-23 17:28:16 +0800

    测试运行6

---
+++ learning-blog/dist/precache-manifest.5a6a0918c99ca005790df0640af31fb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bad6c00c7655278f30f",
+    "url": "css/app.b037abe9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e4022d222bb2b40609a0d227016050f5",
+    "url": "index.html"
+  },
+  {
+    "revision": "c17b903016c2db9c23d6",
+    "url": "js/about.5559224f.js"
+  },
+  {
+    "revision": "9bad6c00c7655278f30f",
+    "url": "js/app.27163f3c.js"
+  },
+  {
+    "revision": "45e13053ec1f58887475",
+    "url": "js/chunk-vendors.ade13aea.js"
+  },
+  {
+    "revision": "80e52eb243f7cf52039528998ef0102d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
